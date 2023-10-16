dd0d0df97d6ca2734170b02332bdcaa2b8b426b7
Molx32 <bonnetclement32@gmail.com>
2023-10-16 15:51:13 +0200

    Add instagram

--- modules/target/target_receive_smss.py
+++ modules/target/target_receive_smss.py
@@ -9,6 +9,7 @@ from database.database import DatabaseInterface
 from database.models import SMS
 from config.config import Phone
 from config.config import Logger
+from config.config import Config
 # Web
 import pandas as pd
 import requests
@@ -18,12 +19,9 @@ from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
 
-# from selenium_firefox import Firefox
-
 class ReceiveSMSS:    
     def __init__(self):
         self.base_url = "https://receive-smss.com"
-        self.parser_insta   = InstagramParser()
         self.phone_urls     = []
 
         self.smss_new = {}
@@ -34,12 +32,9 @@ class ReceiveSMSS:
         # Run the worker
         while 1:
             try:
-                # start = time.time()
                 self.fetch_phones()
-                # end = time.time()
-                # Logger.log('MAIN - FETCH PHONES - ' + str(end - start))
-
                 self.fetch_smss()
+                self.fetch_instagram()
 
                 self.populate_database()
             except Exception as e:
@@ -70,10 +65,11 @@ class ReceiveSMSS:
             self.smss_hist[phone_url] = smss_temp
 
     def fetch_instagram(self):
-        # # Check if message contains website
-        for sms in smss:
-            pass
-            # self.parse_instagram(sms)
+        # Check if message contains website
+        Logger.log('FETCH INSTAGRAM CALL - ' + str(len(self.smss_new)))
+        for phone_url in self.smss_new: 
+            for sms in self.smss_new[phone_url]:
+                self.parse_instagram(sms)
 
     def populate_database(self):
         # # Insert new SMSs into database
@@ -89,21 +85,24 @@ class ReceiveSMSS:
                 Logger.log('DATABASE EXCEPTION - ' + phone_url + ' - ' + str(e))
                 continue
 
-    def send_request_get(self, url):
-        headers = {
-            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 OPR/101.0.0.0'
-        }
-        return requests.get(url, headers=headers)
-
     def parse_instagram(self, sms):
-        regex = r"https://ig.me/\w+"
-        l = re.findall(regex, sms.msg)
-        if l:
-            # Parse
-            print(l[0])
-            r = self.send_request_get(l[0])
-            add_instagram_account = self.parser_insta.parse(r.text)
-            sms.add_instagram_account(add_instagram_account)
+        # Case 1
+        #   "Tap to reset your Instagram p***word: https://ig.me/1ZZOeHuLUwa4Ttq"
+        #   There is no username, and it is not necessary to reset the password to access the account
+        # Case 2
+        #   Instagram link: https://ig.me/r6LPOYK87HRyT73. Don't share it.
+        #   Password reset for a given account
+        try:
+            regex = r"https://ig.me/\w+"
+            l = re.findall(regex, sms.msg)
+            if l:
+                Logger.log('PARSE INSTAGRAM CALL PATTERN - ' + str(l[0]))
+                # Parse
+                url = l[0]
+                r = self.browser.parse_instagram(url)
+                sms.add_instagram_account(r)
+        except Exception as e:
+            Logger.log('EXCEPTION - PARSE INSTAGRAM CALL PATTERN - ' + str(e))
 
 class Browser:
     def __init__(self, name, url):
@@ -131,10 +130,10 @@ class Browser:
         self.browser = webdriver.Firefox(options=FIREFOX_OPTS)
 
         # Parsers
-        self.parser_insta   = InstagramParser()
 
         # Data
         self.class_a    = "number-boxes1-item-button number-boxess1-item-button button blue stroke rounded tssb"
+        self.class_username = "x1lliihq x1plvlek xryxfnj x1n2onr6 x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye xl565be x1s688f x5n08af x1tu3fi x3x7a5m x10wh9bi x1wdrske x8viiok x18hxmgj"
 
         self.class_sender = "col-md-3 sender"
         self.class_msg    = "col-md-6 msg"
@@ -233,23 +232,17 @@ class Browser:
             if receiver.startswith(country['code'].replace('+','')):
                 return country['name']
 
-class InstagramParser:
+    def parse_instagram(self, url):
+        try:
+            self.browser.get(url)
 
-    def __init__(self):
-        self.class_username = "x1lliihq x1plvlek xryxfnj x1n2onr6 x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye xl565be x1s688f x5n08af x1tu3fi x3x7a5m x10wh9bi x1wdrske x8viiok x18hxmgj"
-        self.soup = None
-
-    def parse(self, html):
-        # Parse with BeautifulSoup
-        username = ""
-        self.soup = BeautifulSoup(html, features="lxml")
-        
-        for span in self.soup.find_all('span'):
-            try:
+            # Parse
+            soup = BeautifulSoup(self.browser.page_source, features="lxml")
+            for span in soup.find_all('span'):
                 if self.class_username in " ".join(span.get('class')):
-                    print(span)
                     username = span.get_text()
-                    print(username)
-            except:
-                print("error")
-                pass
+                    Logger.log('PARSE INSTAGRAM - USERNAME - ' + username)
+                    return username
+            return "NOT FOUND"
+        except Exception as e:
+            Logger.log("PARSE INSTAGRAM - USERNAME - " + str(e))
